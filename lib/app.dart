4d3fa6fa459b1c05c65a27c9a920135814568d33
kr-pruto2 <dafg1254@gmail.com>
2026-07-18 12:23:57 +0900

    app.dart 업데이트

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,24 @@
 //src 폴더를 생성해서 그 안에 넣을 것 
 
+import 'package:umicat_app/main.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
 	const App({super.key});
+	
+	@override
+	state<app> createState() => _Appstate();
+}
+class _Appstate extends State<App>{
+	late bool isInitStarted;
+	
+	@override
+	void initState() {
+		super.initState();
+		isInitStarted = prefs.getBool('isInitStarted') ?? true;
+	}
 	@override
 	Widget build(BuildContext context) {
-		return Scaffold(
-			body: Center(
-				child: Image.asset('asstes/images/logo_simbol.png'),   //로고 사진 파일명 바꿀 필요 있음
-			),
-		);
+		return isInitStarted ? const InitStartPage() : const SplashPage();
 	}
 }
