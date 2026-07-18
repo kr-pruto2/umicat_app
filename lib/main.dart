af4662aab6bb942a144a2555f02dfd413963b43d
kr-pruto2 <dafg1254@gmail.com>
2026-07-18 11:21:03 +0900

    main.dart 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+late SharedPreferences prefs;
+void main() async {
+	WidgetsFlutterBinding.ensureInitialized();
+	prefs = await SharedPreferences.getInstance();
+	await Firebase.initializeApp(							//책의 절차를 따라 visual studio에서 firebase 로그인을 한 후 사용가능
+		options: DefaultFirebaseOptions.currentPlatform,
+	);
+
+	runApp(const Myapp());
+}
+
 class MyApp extends StatelessWidget{
 	const MyApp({super.key});
 	@override
